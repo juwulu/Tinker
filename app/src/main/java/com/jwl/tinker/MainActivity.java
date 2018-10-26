df8002ac1f77ba17505e905173592acc873742c2
juwulu <junwulu@163.com>
2018-10-26 16:00:14 +0800

    Initial commit

---
+++ app/src/main/java/com/jwl/tinker/MainActivity.java
@@ -0,0 +1,19 @@
+package com.jwl.tinker;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_LOGS},100);
+        }
+    }
+}
---
+++ app/src/main/java/com/jwl/tinker/SampleApplication.java
@@ -0,0 +1,11 @@
+package com.jwl.tinker;
+
+import com.tencent.tinker.loader.app.TinkerApplication;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+public class SampleApplication extends TinkerApplication {
+    public SampleApplication() {
+        super(ShareConstants.TINKER_ENABLE_ALL, "com.jwl.tinker.SampleApplicationLike",
+                "com.tencent.tinker.loader.TinkerLoader", false);
+    }
+}
